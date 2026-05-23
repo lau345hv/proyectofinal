3c4006ed666b63b107eec3d973a37e074519f4b8
JesusSolarte <jesus.solarte622@gmail.com>
2026-05-23 02:12:15 -0500

    Test Solucion DeepSource backend 5
    Deep source le genera error a ProyectofinalApplicationTests porque estaba vacío

--- proyectofinal/src/test/java/co/edu/unbosque/proyectofinal/ProyectofinalApplicationTests.java
+++ proyectofinal/src/test/java/co/edu/unbosque/proyectofinal/ProyectofinalApplicationTests.java
@@ -1,21 +1,26 @@
 package co.edu.unbosque.proyectofinal;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Pruebas de arranque de la aplicación Spring Boot.
  *
  * @author Equipo de desarrollo
- * @version 1.0
+ * @version 1.1
  */
 @SpringBootTest
 class ProyectofinalApplicationTests {
 
-	/**
-	 * Verifica que el contexto de Spring se inicialice correctamente.
-	 */
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context, "El contexto de Spring no debería ser nulo.");
+    }
 }

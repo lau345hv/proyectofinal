f55584c6641fb4c0333d711430799befbae50d84
JesusSolarte <jesus.solarte622@gmail.com>
2026-05-23 01:51:25 -0500

    Solucion DeepSouerce Backend 3
    Solucion DeepSouerce Backend 3

--- proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/controller/UsuarioController.java
+++ proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/controller/UsuarioController.java
@@ -142,6 +142,9 @@ public class UsuarioController {
 		return new ResponseEntity<>("Usuario actualizado correctamente.", HttpStatus.OK);
 	}
 
+	// =====================================================================
+	// ENDPOINTS SOLO PARA ADMIN
+	// =====================================================================
 
 	@GetMapping("/listar")
 	@Operation(summary = "Listar todos los usuarios (SOLO ADMIN)")
